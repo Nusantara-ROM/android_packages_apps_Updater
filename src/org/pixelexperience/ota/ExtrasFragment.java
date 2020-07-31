i-o-n/packages_apps_Updates

Updates: update icons & add more links

src/com/ion/ota/ExtrasFragment.java
@@ -21,17 +21,23 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
     private static final String WEBSITE_INFO = "website_info";
     private static final String NEWS_INFO = "news_info";
     private static final String DONATE_INFO = "donate_info";
+    private static final String DEVELOPER_INFO = "developer_info";
+    private static final String SUPPORT_INFO = "support_info";
+    private static String DEVELOPER_URL = "https://github.com/Ankit-Gourav";
     private static String MAINTAINER_URL = "";
     private static String DONATE_URL = "";
     private static String FORUM_URL = "";
     private static String WEBSITE_URL = "";
     private static String NEWS_URL = "";
+    private static String SUPPORT_URL = "https://paypal.me/AnkitGourav";
     private PreferenceCategory mExtrasCategory;
     private Preference mMaintainerInfo;
     private Preference mForumInfo;
     private Preference mWebsiteInfo;
     private Preference mNewsInfo;
     private Preference mDonateInfo;
+    private Preference mDeveloperInfo;
+    private Preference mSupportInfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,12 +49,16 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
         mWebsiteInfo = findPreference(WEBSITE_INFO);
         mNewsInfo = findPreference(NEWS_INFO);
         mDonateInfo = findPreference(DONATE_INFO);
+        mDeveloperInfo = findPreference(DEVELOPER_INFO);
+        mSupportInfo = findPreference(SUPPORT_INFO);
         mMaintainerInfo.setOnPreferenceClickListener(this);
         mForumInfo.setOnPreferenceClickListener(this);
         mWebsiteInfo.setOnPreferenceClickListener(this);
         mNewsInfo.setOnPreferenceClickListener(this);
         mDonateInfo.setOnPreferenceClickListener(this);
         getPreferenceScreen().removeAll();
+        mDeveloperInfo.setOnPreferenceClickListener(this);
+        mSupportInfo.setOnPreferenceClickListener(this);
     }
 
     public void updatePrefs(UpdateInfo update) {
@@ -60,6 +70,8 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
         }
         getPreferenceScreen().addPreference(mExtrasCategory);
         mExtrasCategory.removeAll();
+        mExtrasCategory.addPreference(mDeveloperInfo);
+        mExtrasCategory.addPreference(mSupportInfo);
         if (update.getMaintainer() != null && !update.getMaintainer().isEmpty()) {
             mMaintainerInfo.setSummary(update.getMaintainer());
             mExtrasCategory.addPreference(mMaintainerInfo);
@@ -122,6 +134,18 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
                 showSnackbar(R.string.error_open_url, Snackbar.LENGTH_SHORT);
             }
             return true;
+        } else if (preference == mDeveloperInfo) {
+            if (DEVELOPER_URL.equals("")) {
+                return true;
+            }
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEVELOPER_URL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (Exception ex) {
+                showSnackbar(R.string.error_open_url, Snackbar.LENGTH_SHORT);
+            }
+            return true;
         } else if (preference == mForumInfo) {
             try {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(FORUM_URL));
@@ -152,6 +176,15 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
         } else if (preference == mDonateInfo) {
             try {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DONATE_URL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (Exception ex) {
+                showSnackbar(R.string.error_open_url, Snackbar.LENGTH_SHORT);
+            }
+            return true;
+        } else if (preference == mSupportInfo) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(SUPPORT_URL));
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             } catch (Exception ex) {

Nusantara-ROM/android_packages_apps_Updater

src/org/pixelexperience/ota/ExtrasFragment.java
@@ -21,17 +21,23 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
     private static final String WEBSITE_INFO = "website_info";
     private static final String NEWS_INFO = "news_info";
     private static final String DONATE_INFO = "donate_info";
+    private static final String DEVELOPER_INFO = "developer_info";
+    private static final String SUPPORT_INFO = "support_info";
+    private static String DEVELOPER_URL = "https://github.com/Genkzsz11";
     private static String MAINTAINER_URL = "";
     private static String DONATE_URL = "";
     private static String FORUM_URL = "";
     private static String WEBSITE_URL = "";
     private static String NEWS_URL = "";
+    private static String SUPPORT_URL = "https://paypal.me/";
     private PreferenceCategory mExtrasCategory;
     private Preference mMaintainerInfo;
     private Preference mForumInfo;
     private Preference mWebsiteInfo;
     private Preference mNewsInfo;
     private Preference mDonateInfo;
+    private Preference mDeveloperInfo;
+    private Preference mSupportInfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,12 +49,16 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
         mWebsiteInfo = findPreference(WEBSITE_INFO);
         mNewsInfo = findPreference(NEWS_INFO);
         mDonateInfo = findPreference(DONATE_INFO);
+        mDeveloperInfo = findPreference(DEVELOPER_INFO);
+        mSupportInfo = findPreference(SUPPORT_INFO);
         mMaintainerInfo.setOnPreferenceClickListener(this);
         mForumInfo.setOnPreferenceClickListener(this);
         mWebsiteInfo.setOnPreferenceClickListener(this);
         mNewsInfo.setOnPreferenceClickListener(this);
         mDonateInfo.setOnPreferenceClickListener(this);
         getPreferenceScreen().removeAll();
+        mDeveloperInfo.setOnPreferenceClickListener(this);
+        mSupportInfo.setOnPreferenceClickListener(this);
     }
 
     public void updatePrefs(UpdateInfo update) {
@@ -60,6 +70,8 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
         }
         getPreferenceScreen().addPreference(mExtrasCategory);
         mExtrasCategory.removeAll();
+        mExtrasCategory.addPreference(mDeveloperInfo);
+        mExtrasCategory.addPreference(mSupportInfo);
         if (update.getMaintainer() != null && !update.getMaintainer().isEmpty()) {
             mMaintainerInfo.setSummary(update.getMaintainer());
             mExtrasCategory.addPreference(mMaintainerInfo);
@@ -122,6 +134,18 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
                 showSnackbar(R.string.error_open_url, Snackbar.LENGTH_SHORT);
             }
             return true;
+        } else if (preference == mDeveloperInfo) {
+            if (DEVELOPER_URL.equals("")) {
+                return true;
+            }
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEVELOPER_URL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (Exception ex) {
+                showSnackbar(R.string.error_open_url, Snackbar.LENGTH_SHORT);
+            }
+            return true;
         } else if (preference == mForumInfo) {
             try {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(FORUM_URL));
@@ -152,6 +176,15 @@ public class ExtrasFragment extends PreferenceFragment implements Preference.OnP
         } else if (preference == mDonateInfo) {
             try {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DONATE_URL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (Exception ex) {
+                showSnackbar(R.string.error_open_url, Snackbar.LENGTH_SHORT);
+            }
+            return true;
+        } else if (preference == mSupportInfo) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(SUPPORT_URL));
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             } catch (Exception ex) {